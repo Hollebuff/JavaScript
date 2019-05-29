76324a591449528d3327b180a392816b945a0cd5
Hollebuff <763651741@qq.com>
2019-05-30 06:51:47 +0800

    Updates

---
+++ precache-manifest.3e6aa3af60d4daf56bcfb097ccfff4a2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06da5c906913b82e84e9b90dc7616920",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb30ba1d035be6314114",
+    "url": "./static/css/main.65966b8e.chunk.css"
+  },
+  {
+    "revision": "496b5778806c2bff2801",
+    "url": "./static/js/2.0ca30bb9.chunk.js"
+  },
+  {
+    "revision": "bb30ba1d035be6314114",
+    "url": "./static/js/main.42ff7cd7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bb2d77937c3b675f9e463643534e633f",
+    "url": "./static/media/bannar1.bb2d7793.jpg"
+  },
+  {
+    "revision": "3832007785772548fb4359ccc99033f9",
+    "url": "./static/media/bannar2.38320077.jpg"
+  },
+  {
+    "revision": "6f27b317c727d1ea819672837f367398",
+    "url": "./static/media/bannar3.6f27b317.jpg"
+  },
+  {
+    "revision": "ba9d5caae4c776cc34657493fcdb7215",
+    "url": "./static/media/bannar4.ba9d5caa.jpg"
+  },
+  {
+    "revision": "30a1d480dd30d8de52b23af576be8c48",
+    "url": "./static/media/button2.30a1d480.png"
+  },
+  {
+    "revision": "6cb90dbc94e586bfc5a53d5410b1cac2",
+    "url": "./static/media/coverall.6cb90dbc.png"
+  },
+  {
+    "revision": "48c00478c491e945ee26b282420f8ec5",
+    "url": "./static/media/download.48c00478.png"
+  },
+  {
+    "revision": "cea28c5441f53411124b34366d6775d3",
+    "url": "./static/media/foot_enter_new.cea28c54.png"
+  },
+  {
+    "revision": "29cd8439e24c97d0d55389704ac02e70",
+    "url": "./static/media/icon.29cd8439.png"
+  },
+  {
+    "revision": "de77152ca400e40498b545c021cf3bb0",
+    "url": "./static/media/iconall.de77152c.png"
+  },
+  {
+    "revision": "5556d9cf1a530e492c5f1672a71918aa",
+    "url": "./static/media/index.5556d9cf.png"
+  },
+  {
+    "revision": "fb6cb3106ea8fb6c1e08b161f5d006ed",
+    "url": "./static/media/new1.fb6cb310.jpg"
+  },
+  {
+    "revision": "db568b1cb780bab0b4543a10a93c5d4b",
+    "url": "./static/media/playbar.db568b1c.png"
+  },
+  {
+    "revision": "4aec0aacf2d45aa9382642f63c30f3d7",
+    "url": "./static/media/sprite.4aec0aac.png"
+  },
+  {
+    "revision": "721593c0e999e07162a5e012f90599cd",
+    "url": "./static/media/toplist.721593c0.png"
+  }
+]);
